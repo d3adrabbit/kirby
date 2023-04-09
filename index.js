c41be0cc93147e2e999b9cf6635edaadc929e3c9
DeadRabbit <309358965@qq.com>
2023-04-09 13:01:49 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
-import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
-import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
 export function main() {
   const canvas = document.querySelector("#canvas");
